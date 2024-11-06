d32a7d5c30f8351e3d05062e6f54b4b5662341ba
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-11-05 19:52:42 -0800

    practicing React.

--- src/assets/DoubleDice.jsx
+++ src/assets/DoubleDice.jsx
@@ -3,6 +3,7 @@ export default function DoubleDice(){
     const num2 = Math.floor(Math.random() * 3) + 1;
     return (
         <div>
+        <h2></h2>
         <p>Num1: {num1}</p>
         <p>Num1: {num2}</p>
         </div>
