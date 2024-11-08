54cf8d29590876605e95013556cf8181b0272335
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-11-08 14:23:14 -0800

    created slot machine exercise with react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import Slots from "./Slots"
 import './App.css'
-import Greeter from "./Greeter"
-import ListPicker from './ListPicker'
+// import Greeter from "./Greeter"
+// import ListPicker from './ListPicker'
 
 function App() {
  return ( 
  <div>
+    <Slots val1="🍒" val2="🍒" val3="🍒"/>
+    <Slots val1="🍒" val2="🍌" val3="🍉"/>
+    
+    
+    
+    
+    
+    
+    
     {/* <Greeter person="Bob"/>
     <Greeter person="Jim"/>
     <Greeter person="Toby"/>
     <Greeter person="Luke"/> */}
-    <ListPicker values={[1,2,3,4,5,6,7]}/>
-    <ListPicker values={[1,2,3,4,5,6,7]}/>
-    <ListPicker values={[1,2,3,4,5,6,7]}/>
+    {/* <ListPicker values={[1,2,3,4,5,6,7]}/> */}
   </div>
 )}
 
--- src/SlotMachine.jsx
+++
@@ -1,3 +0,0 @@
-export default function SlotMachine(){
-    const val1 = Math.floor(Math.random() * 3) + 1;
-}
