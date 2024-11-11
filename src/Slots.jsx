3bd0976b1cbbdec3476540b15efe4e3d5f3e7c04
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-11-10 23:37:05 -0800

    writing React conditional statements

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,5 +1,5 @@
 export default function Slots({val1, val2, val3}){
-    const isWinner = val1 === val2 && val1 ===val3;
+    const isWinner = val1 === val2 && val1 === val3;
     
     return (
         <div>
@@ -7,7 +7,7 @@ export default function Slots({val1, val2, val3}){
             <h2 style={{color: isWinner ? "green" : "red"}}>
                 {isWinner ? "You Win!" : "You lose"}
             </h2>
-            {isWinner && <h3>"Congrats"</h3>}
+            {isWinner && <h3>"Congratulations</h3>}
         </div>
     )
 }
