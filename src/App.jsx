738383227659e3d3e6723c035b04ebb2926c33bb
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-12-05 17:08:16 -0800

    exercise with React useState hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Slots from "./Slots";
-import PropertyList from "./PropertyList";
+import Counter from './Counter';
+// import Clicker from './Clicker';
+// import PropertyList from "./PropertyList";
 // import ShoppingList from './ShoppingList';
 // import Greeter from "./Greeter"
 // import ListPicker from './ListPicker'
@@ -12,19 +14,23 @@ import PropertyList from "./PropertyList";
 //   {id: 4, item: 'carrots', quantity: 12, completed: true}
 // ]
 
-const properties = [
-  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-];
+// const properties = [
+//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+// ];
 
 function App() {
  return ( 
   <div>
-    <PropertyList properties={properties} />  
+    <Counter num={1}/>
+
+    {/* <PropertyList properties={properties} />   */}
+  {/* <Clicker message="HI!!!!!" buttonText="PLEASE CLICK ME"/>
+  <Clicker message="PLEASE STOP CLICKING ME" buttonText="DON'T CLICK ME "/> */}
   </div>
 )}
 
